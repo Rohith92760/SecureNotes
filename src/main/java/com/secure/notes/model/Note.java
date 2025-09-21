e5268f45378ac85c4c7b27449f234b30e0b74bba
Rohith <Rohithpottabathini@gmail.com>
2025-09-21 13:21:40 +0530

    First Commit

---
+++ src/main/java/com/secure/notes/model/Note.java
@@ -0,0 +1,21 @@
+package com.secure.notes.model;
+
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long noteId;
+
+    @Lob
+    private String content;
+
+    private String ownerUsername;
+
+}
